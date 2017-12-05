83bc875b5cb75802c13d24deba28e49d3b8242ea
yohith <33994861+yohith@users.noreply.github.com>
2017-12-05 12:16:39 +0530

    Lower to higher using lambda function

---
+++ Lower to higher.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  5 12:10:16 2017
+
+@author: Rengaprasad
+"""
+
+nums = ["Raj","Ram","Ravi","raghul"]
+nums = list(map(lambda x:x.lower(),nums))
+print (nums)
